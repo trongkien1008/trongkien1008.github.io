a8871235f7013fd020048c33ac5ecfb05f3e50f5
Nguyen Trong Kien <trongkien1008@gmail.com>
2020-05-12 00:42:24 +0700

    update click to top

--- js/custom.js
+++ js/custom.js
@@ -163,3 +163,29 @@ $(document).ready(function () {
 
     });
 });
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    // $(window).scrollTop(0); 
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+    $(this).click(function(){ 
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+        return false; 
+    });
+}
